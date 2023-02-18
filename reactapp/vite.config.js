647d09b6557c1b6e4ef86438378aad5a1d921b4e
AnitaTess <anita.aksentowicz@onet.pl>
2023-02-18 22:15:21 +0000

    PWA

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -4,7 +4,21 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  VitePWA({ registerType: 'autoUpdate' }) ],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "icons/theicon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable"
+          }
 
+        ]
+      },
+      registerType: 'autoUpdate'
+    })
+  ],
+})
